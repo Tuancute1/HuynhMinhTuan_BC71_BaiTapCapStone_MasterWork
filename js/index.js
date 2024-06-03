15609e885992ef6aa9e6d604d47e33a4eab040e7
Tuancute1 <huynhminhtuan13032003@gmail.com>
2024-06-03 15:22:50 +0700

    xong

---
+++ js/index.js
@@ -0,0 +1,46 @@
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        document.getElementById('sun').style.opacity = '0';
+        document.getElementById('sun').style.transform = 'scale(0)';
+        document.getElementById('moon').style.opacity = '1';
+        document.getElementById('moon').style.transform = 'scale(1)';
+    } else {
+        document.getElementById('sun').style.opacity = '1';
+        document.getElementById('sun').style.transform = 'scale(1)';
+        document.getElementById('moon').style.opacity = '0';
+        document.getElementById('moon').style.transform = 'scale(0)';
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        document.getElementById('sun').style.opacity = '0';
+        document.getElementById('sun').style.transform = 'scale(0)';
+        document.getElementById('moon').style.opacity = '1';
+        document.getElementById('moon').style.transform = 'scale(1)';
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        document.getElementById('sun').style.opacity = '1';
+        document.getElementById('sun').style.transform = 'scale(1)';
+        document.getElementById('moon').style.opacity = '0';
+        document.getElementById('moon').style.transform = 'scale(0)';
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+// SEARCH
+document.addEventListener("DOMContentLoaded", function(){
+    var masSearch = document.getElementById("masSearch");
+    
+});
